a80af9f0e8d65bc2eee378405b310ed68cae2d6d
Zeyu Kuang <kzy2142@gmail.com>
2017-09-17 09:23:44 +0800

    Revert "Init"
    This reverts commit d567514e68274c3116057205e6c7a9de40c94a33.

--- gold sphere scattering/matlab/My Mie Program/Mie_sphere/archieve/Version 1/Qabs_vs_wavelength_vs_a.m
+++
@@ -1,8 +0,0 @@
-close all
-for a = 0.01:0.01:0.1
-    Qabs_vs_wavelength
-    hold on
-end
-legend('a = 0.01um','a = 0.02um','a = 0.03um','a = 0.04um'...
-,'a = 0.05um','a = 0.06um','a = 0.07um'...
-,'a = 0.08um','a = 0.09um','a = 0.10um')
